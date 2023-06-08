af6b82a84db6dfa6d1a14f2622182b20f389d42c
RubelAzad <rubelazad123@gmail.com>
2023-06-08 11:42:01 +0600

    serach modify with out name

--- app/Http/Controllers/SearchPatientController.php
+++ app/Http/Controllers/SearchPatientController.php
@@ -4,129 +4,174 @@ namespace App\Http\Controllers;
 use Symfony\Component\HttpFoundation\Response as HttpResponse;
 use Illuminate\Http\Request;
 use App\Models\Patient;
+use App\Models\Station;
 class SearchPatientController extends Controller
 {
-    public function searchPatient(Request $request){
-
-        $searchQuery = $request->query('search');
-        $searchOption = $request->query('option');
-
-        $query = Patient::query();
-
-        if ($searchQuery && $searchOption) {
-            $query->where($searchOption, 'like', '%' . $searchQuery . '%');
-        }
-
-        $data = $query->get();
-
-        return response()->json($data);
-        
-    }
-
     public function searchPatientAllInfo(Request $request){
         $stationId=$request->station;
-        $stationId=$request->station;
+        $Card=$request->Card;
+        $Name=$request->Name;
+        $NID=$request->NID;
+        $Mobile=$request->Mobile;
+
+
+        $patientDetails=Patient::select('PatientId')->where('RegistrationId','=',$Card)->orWhere('IdNumber','=',$NID)->orWhere('CellNumber','=',$Mobile)->first();
+
+        $StationStatus = Station::select('StationStatus')->where('PatientId','=',$patientDetails->PatientId)->first();
+
+        if($StationStatus->StationStatus === $stationId){
+            $patientDetails=Patient::with('Gender','MartitalStatus')->where('PatientId','=',$patientDetails->PatientId)->get();
+            return $patientDetails;
+        }else{
+            return 'Please Check Station - '.$StationStatus->StationStatus;
+        }
+        
+        
+
+
+
+
+
         if($stationId == ""){
             return $this->responseJson(false, HttpResponse::HTTP_BAD_GATEWAY, 'Error. Could Not Found Station Id');
         }else{
             if($stationId == "1"){
                 if($request->Card){
                     $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->Card)->get();
-                    return $patientDetails;
-                }
-                if($request->NID){
-                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('IdNumber','=',$request->NID)->get();
-                    return $patientDetails;
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
                 }
                 if($request->Name){
-                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('GivenName','=',$request->Name)->get();
-                    return $patientDetails;
+                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->Name)->get();
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
+                }
+                if($request->NID){
+                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->NID)->get();
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
                 }
                 if($request->Mobile){
-                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('CellNumber','=',$request->Mobile)->get();
-                    return $patientDetails;
+                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->Mobile)->get();
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
                 }
-    
-            }elseif($stationId == "2"){
+            }
+            if($stationId == "2"){
                 if($request->Card){
                     $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->Card)->get();
-                    return $patientDetails;
-                }
-                if($request->NID){
-                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('IdNumber','=',$request->NID)->get();
-                    return $patientDetails;
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
                 }
                 if($request->Name){
-                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('GivenName','=',$request->Name)->get();
-                    return $patientDetails;
+                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->Name)->get();
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
+                }
+                if($request->NID){
+                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->NID)->get();
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
                 }
                 if($request->Mobile){
-                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('CellNumber','=',$request->Mobile)->get();
-                    return $patientDetails;
+                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->Mobile)->get();
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
                 }
-    
-            }elseif($stationId == "3"){
+            }
+            if($stationId == "3"){
                 if($request->Card){
                     $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->Card)->get();
-                    return $patientDetails;
-                }
-                if($request->NID){
-                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('IdNumber','=',$request->NID)->get();
-                    return $patientDetails;
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
                 }
                 if($request->Name){
-                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('GivenName','=',$request->Name)->get();
-                    return $patientDetails;
+                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->Name)->get();
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
+                }
+                if($request->NID){
+                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->NID)->get();
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
                 }
                 if($request->Mobile){
-                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('CellNumber','=',$request->Mobile)->get();
-                    return $patientDetails;
+                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->Mobile)->get();
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
                 }
-    
-            }elseif($stationId == "4"){
-                if($request->patientId){
-                    $patientDetails=Patient::with('Gender','MartitalStatus','bps','height_weights','glucose_hbs')->where('patientId','=',$request->patientId)->get();
-                    return response()->json(['status' => true, 'code'=>200, 'message'=>'Get Patient Data','patientAllInfo'=>$patientDetails], 200);
-                }
+            }
+            if($stationId == "4"){
                 if($request->Card){
                     $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->Card)->get();
-                    return response()->json(['status' => true, 'code'=>200, 'message'=>'Get Patient Data','patientAllInfo'=>$patientDetails], 200);
-                }
-                if($request->NID){
-                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('IdNumber','=',$request->NID)->get();
-                    return response()->json(['status' => true, 'code'=>200, 'message'=>'Get Patient Data','patientAllInfo'=>$patientDetails], 200);
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
                 }
                 if($request->Name){
-                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('GivenName','=',$request->Name)->get();
-                    return response()->json(['status' => true, 'code'=>200, 'message'=>'Get Patient Data','patientAllInfo'=>$patientDetails], 200);
+                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->Name)->get();
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
+                }
+                if($request->NID){
+                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->NID)->get();
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
                 }
                 if($request->Mobile){
-                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('CellNumber','=',$request->Mobile)->get();
-                    return response()->json(['status' => true, 'code'=>200, 'message'=>'Get Patient Data','patientAllInfo'=>$patientDetails], 200);
+                    $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->Mobile)->get();
+                    if(count($patientDetails) === 0){
+                        return 'Station 1 User data not found, This Patient is not Registered.';
+                    }else{
+                        return $patientDetails;
+                    }
                 }
-    
-            }else{
-                return $this->responseJson(false, HttpResponse::HTTP_BAD_GATEWAY, 'Error. Could Not Found Data');
             }
         }
-
-        
-        
-        // if($request->Card){
-        //     $patientDetails=Patient::with('Gender','MartitalStatus')->where('RegistrationId','=',$request->Card)->get();
-        //     return $patientDetails;
-        // }
-        // if($request->NID){
-        //     $patientDetails=Patient::with('Gender','MartitalStatus')->where('IdNumber','=',$request->NID)->get();
-        //     return $patientDetails;
-        // }
-        // if($request->Name){
-        //     $patientDetails=Patient::with('Gender','MartitalStatus')->where('GivenName','=',$request->Name)->get();
-        //     return $patientDetails;
-        // }
-        // if($request->Mobile){
-        //     $patientDetails=Patient::with('Gender','MartitalStatus')->where('CellNumber','=',$request->Mobile)->get();
-        //     return $patientDetails;
-        // }
+                    
     }
 }
