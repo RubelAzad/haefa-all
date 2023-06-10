85409d0f1318a3e438b5138fab94ee3a3170eabd
RubelAzad <rubelazad123@gmail.com>
2023-06-10 14:57:53 +0600

    patient address update

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Patient;
 class Address extends Model
 {
     protected $table = 'Address';
     public $timestamps = false;
     protected $fillable = [ 'PatientId', 'AddressLine1', 'AddressLine1Parmanent','AddressLine2','AddressLine2Parmanent','Village','VillageParmanent','Thana','ThanaParmanent','PostCode','PostCodeParmanent','District','DistrictParmanent','Country','CountryParmanent','Status','Camp', 'BlockNumber', 'Majhi', 'TentNumber', 'FCN','OrgId','CreateUser','CreateDate','UpdateUser','UpdateDate'];
+
+    public function patient()
+    {
+      return $this->belongsTo(Patient::class, 'PatientId', 'PatientId');
+    }
 }
