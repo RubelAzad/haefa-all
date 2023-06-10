0116761876c5a4c01aafda860b4ed1e22a8f5efc
RubelAzad <rubelazad123@gmail.com>
2023-06-10 10:08:58 +0600

    patient info update 3 station all

--- app/Http/Controllers/SearchPatientController.php
+++ app/Http/Controllers/SearchPatientController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 use Symfony\Component\HttpFoundation\Response as HttpResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Models\Patient;
 use App\Models\Station;
 class SearchPatientController extends Controller
@@ -22,48 +21,8 @@ class SearchPatientController extends Controller
 
         if($StationStatus->StationStatus === $stationId){
             if($StationStatus->StationStatus === "4"){
-                $PatientDetails= DB::select("SELECT PD.RegistrationId AS RegistrationId, PD.GivenName AS GivenName, PD.FamilyName AS FamilyName, PD.BirthDate AS BirthDate, PD.Age AS Age, PD.IdNumber AS IdNumber, PD.CellNumber AS CellNumber, RG.GenderCode AS GenderCode, RMS.MaritalStatusCode AS MaritalStatusCode
-                FROM Patient as PD
-                INNER JOIN RefGender as RG on RG.GenderId = PD.GenderId
-                INNER JOIN RefMaritalStatus as RMS on RMS.MaritalStatusId = PD.MaritalStatusId
-                WHERE PatientId = '$patientDetails->PatientId'");
-
-                $HeightWeight= DB::select("SELECT TOP 1 MAX(Height) AS Height, MAX(Weight) AS Weight, MAX(BMI) AS BMI, MAX(BMIStatus) AS BMIStatus, CAST(CreateDate AS date) as CreateDate
-                FROM MDataHeightWeight WHERE PatientId = '$patientDetails->PatientId' AND CAST(CreateDate AS date) 
-                = CAST(
-                    (SELECT TOP 1 MAX(CreateDate) AS MaxCreateDate
-                    FROM MDataHeightWeight WHERE PatientId = '$patientDetails->PatientId'
-                    GROUP BY CAST(CreateDate AS date)
-                    ORDER BY MaxCreateDate DESC)
-                    AS date) GROUP BY CreateDate ORDER BY CreateDate");
-
-                $BP= DB::select("SELECT TOP 1 MAX(BPSystolic1) AS BPSystolic1, MAX(BPDiastolic1) AS BPDiastolic1, MAX(BPSystolic2) AS BPSystolic2, MAX(BPDiastolic2) AS BPDiastolic2, MAX(HeartRate) AS HeartRate, MAX(CurrentTemparature) AS CurrentTemparature, CAST(CreateDate AS date) as CreateDate
-                FROM MDataBP WHERE PatientId = '$patientDetails->PatientId' AND CAST(CreateDate AS date) 
-                = CAST(
-                    (SELECT TOP 1 MAX(CreateDate) AS MaxCreateDate
-                    FROM MDataBP WHERE PatientId = '$patientDetails->PatientId'
-                    GROUP BY CAST(CreateDate AS date)
-                    ORDER BY MaxCreateDate DESC)
-                    AS date) GROUP BY CreateDate ORDER BY CreateDate");
-                
-                $GlucoseHb = DB::select("SELECT TOP 1 MAX(RBG) AS RBG, MAX(FBG) AS FBG, MAX(Hemoglobin) AS Hemoglobin, MAX(HrsFromLastEat) AS HrsFromLastEat, CAST(CreateDate AS date) as CreateDate
-                FROM MDataGlucoseHb WHERE PatientId = '$patientDetails->PatientId' AND CAST(CreateDate AS date) 
-                = CAST(
-                    (SELECT TOP 1 MAX(CreateDate) AS MaxCreateDate
-                    FROM MDataGlucoseHb WHERE PatientId = '$patientDetails->PatientId'
-                    GROUP BY CAST(CreateDate AS date)
-                    ORDER BY MaxCreateDate DESC)
-                    AS date) GROUP BY CreateDate ORDER BY CreateDate");
-
-                return response()->json([
-                    'message' => 'Final Prescription',
-                    'code'=>200,
-                    'PatientDetails'=>$PatientDetails,
-                    'HeightWeight'=>$HeightWeight,
-                    'BP'=>$BP,
-                    'GlucoseHb'=>$GlucoseHb,
-
-                ],200);
+                $patientDetails=Patient::with('Gender','MartitalStatus','bps','height_weights','glucose_hbs')->where('PatientId','=',$patientDetails->PatientId)->get();
+                return $patientDetails;
             }else{
                 $patientDetails=Patient::with('Gender','MartitalStatus')->where('PatientId','=',$patientDetails->PatientId)->get();
                 return $patientDetails;
